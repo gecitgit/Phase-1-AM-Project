0cb1e3d0977bac82e8adcee7e2953ddaa1e628dd
+ <+>
2022-12-27 12:23:14 -0500

    Finalized the data that is pulled by API and tweaked flexbox

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
     formSearch.addEventListener("submit", (event) => {
         event.preventDefault()
         console.log("this is event: ", event.target[0].value)
-        fetch(`https://api.jikan.moe/v4/manga?letter=${event.target[0].value}&sfw&order_by=popularity`)
+        fetch(`https://api.jikan.moe/v4/manga?letter=${event.target[0].value}&order_by=popularity`)
         .then(response => response.json())
         .then(response => {
-            const animeList = document.querySelector("#anime-list")
-            const infoList = document.getElementById("fetched-results")
+            // const animeList = document.querySelector("#anime-list")
+            // const infoList = document.getElementById("fetched-results")
                         
             const animeDataList = response.data.map(item =>{
                 return {
@@ -17,29 +17,64 @@ document.addEventListener("DOMContentLoaded", () => {
                 picture: item.images.jpg.image_url,
                 author: item.authors[0].name,
                 blurb: item.synopsis,
-                score: item.score
+                score: item.score,
+                status: item.status
+                
             }
             }
             )
+            console.log("this is the animedatalist: ", animeDataList)
             
             //creates the divs for each answr
             const animeDivList = animeDataList.map(item =>{
+                //creates the actual div that will hold each result + labels
                 const animeContainer = document.createElement("div");
                 animeContainer.classList.add("container")
                 
+                //creates a picture div inside of the container div and adds the picture
                 const animePictureDiv = document.createElement("div");
                 animeContainer.appendChild(animePictureDiv)
                 const img = document.createElement("img")
                 animePictureDiv.appendChild(img)
                 img.src = item.picture
 
+                //creates a blank div to make flexbox play fair and create a vertical gap between the manga pic and details
+                const animeSpacer = document.createElement("div");
+                animeContainer.appendChild(animeSpacer)
+                animeSpacer.classList.add("animeSpacer")
+                const blankSpace = document.createElement("p")
+                animeSpacer.appendChild(blankSpace)
+                blankSpace.innerText = ""   
+
+                //creates the animeDetails div which will hold all of the info pulled during the fetch request that was stored to animeDataList
                 const animeDetails = document.createElement("div");
                 animeContainer.appendChild(animeDetails)
                 animeDetails.classList.add("animeDetails")
+
+                //this adds a h2 tag which contains the manga's title
                 const title = document.createElement("h2")
                 animeDetails.appendChild(title)
                 title.innerText = item.title
 
+                //this adds a new P tag containing the manga's summary
+                const summary = document.createElement("p")
+                animeDetails.appendChild(summary)
+                summary.innerText = "Summary: "+item.blurb
+                
+                //this adds a new P tag containing the author's information
+                const author = document.createElement("p")
+                animeDetails.appendChild(author)
+                author.innerText = "Author: "+item.author
+
+                //this adds a new P tag containing the manga's rating
+                const score = document.createElement("p")
+                animeDetails.appendChild(score)
+                score.innerText = "Average Rating: " + item.score
+
+                //this adds a new P tag containing the manga's publishing status
+                const status = document.createElement("p")
+                animeDetails.appendChild(status)
+                status.innerText = "Current Status: " + item.status
                 
 
                 return animeContainer
@@ -58,6 +93,7 @@ document.addEventListener("DOMContentLoaded", () => {
             
 
             )
+        // .catch(error => console.log("ERROR"))
 
             
         })
